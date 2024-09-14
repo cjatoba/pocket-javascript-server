1101cd0bb72324227db7f00ce5aaf3ec9c416ec2
cjatoba <claytonjatoba@gmail.com>
2024-09-14 10:33:02 -0300

    feat: create functions and routes

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,6 +1,59 @@
 import fastify from 'fastify'
+import { z } from 'zod'
+import { createGoal } from '../functions/create-goal'
+import {
+  serializerCompiler,
+  validatorCompiler,
+  type ZodTypeProvider,
+} from 'fastify-type-provider-zod'
+import { getWeekPendingGoals } from '../functions/get-week-pending-goals'
+import { createGoalCompletion } from '../functions/create-goal-completion'
 
-const app = fastify()
+const app = fastify().withTypeProvider<ZodTypeProvider>()
+
+app.setValidatorCompiler(validatorCompiler)
+app.setSerializerCompiler(serializerCompiler)
+
+app.get('/pending-goals', async () => {
+  const { pendingGoals } = await getWeekPendingGoals()
+
+  return {
+    pendingGoals,
+  }
+})
+
+app.post(
+  '/goals',
+  {
+    schema: {
+      body: z.object({
+        title: z.string(),
+        desiredWeeklyFrequency: z.number().int().min(1).max(7),
+      }),
+    },
+  },
+  async request => {
+    await createGoal(request.body)
+  }
+)
+
+app.post(
+  '/completions',
+  {
+    schema: {
+      body: z.object({
+        goalId: z.string(),
+      }),
+    },
+  },
+  async request => {
+    const { goalId } = request.body
+
+    await createGoalCompletion ({
+      goalId
+    })
+  }
+)
 
 app
   .listen({
