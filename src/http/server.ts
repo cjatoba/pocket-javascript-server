c9835db3ed0773480a1a38e0f68a2a64b00794cc
_cjatoba <clayton.jatoba@watch.tv.br>
2024-09-14 19:11:32 -0300

    feat: create more function and separe routes

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,64 +1,29 @@
 import fastify from 'fastify'
-import { z } from 'zod'
-import { createGoal } from '../functions/create-goal'
 import {
+  type ZodTypeProvider,
   serializerCompiler,
   validatorCompiler,
-  type ZodTypeProvider,
 } from 'fastify-type-provider-zod'
-import { getWeekPendingGoals } from '../functions/get-week-pending-goals'
-import { createGoalCompletion } from '../functions/create-goal-completion'
+import { createCompletionRoute } from '../routes/create-completion'
+import { createGoalRoute } from '../routes/create-goal'
+import { getPendingGoalsRoute } from '../routes/get-pending-goals'
+import { getWeekSummaryRoute } from '../routes/get-week-summary'
 
+const PORT = 3333
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
 app.setValidatorCompiler(validatorCompiler)
 app.setSerializerCompiler(serializerCompiler)
 
-app.get('/pending-goals', async () => {
-  const { pendingGoals } = await getWeekPendingGoals()
-
-  return {
-    pendingGoals,
-  }
-})
-
-app.post(
-  '/goals',
-  {
-    schema: {
-      body: z.object({
-        title: z.string(),
-        desiredWeeklyFrequency: z.number().int().min(1).max(7),
-      }),
-    },
-  },
-  async request => {
-    await createGoal(request.body)
-  }
-)
-
-app.post(
-  '/completions',
-  {
-    schema: {
-      body: z.object({
-        goalId: z.string(),
-      }),
-    },
-  },
-  async request => {
-    const { goalId } = request.body
-
-    await createGoalCompletion ({
-      goalId
-    })
-  }
-)
+app.register(createGoalRoute)
+app.register(createCompletionRoute)
+app.register(getPendingGoalsRoute)
+app.register(getWeekSummaryRoute)
 
 app
   .listen({
-    port: 3333,
+    port: PORT,
   })
   .then(() => {
-    console.log('HTTP server running')
+    console.log(`HTTP server running in ${PORT} port`)
   })
